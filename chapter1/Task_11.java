613c5e4ccce1b5d26a2dafc0f2a444d51452f995
Nikita Yakimenko <nyakimenko@taxdome.com>
2023-07-26 19:28:29 +0700

    fix tasks 7, 8, 11; add tasks 17-24

--- chapter1/Task_11.java
+++ chapter1/Task_11.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Task_11 {
     public static void main(String[] args) {
         int[] numbers = new int[4];
+        Scanner input = new Scanner(System.in);
 
-        for (byte i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.print("Введите " + (i + 1) + " число: ");
-            Scanner input = new Scanner(System.in);
             numbers[i] = input.nextInt();
         }
 
-        for (byte i = 0; i < numbers.length; i++) {
-            int lastIndex = numbers.length - 1;
-            if (i == lastIndex) {
-                System.out.print(numbers[i]);
-            } else {
-                System.out.print(numbers[i] + "\n");
+        int lastIndex = numbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i != lastIndex) {
+                System.out.print("\n");
             }
         }
     }
---
+++ chapter1/Task_23.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class Task_23 {
+    public static void main(String[] args) {
+        double a = 5.8;
+        double y = (Math.pow(a, 2) + 10) / Math.sqrt(Math.pow(a, 2) + 1);
+        System.out.println("y = " + y);
+    }
+}
